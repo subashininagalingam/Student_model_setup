df46777926518f3778b1922b76e45dc092bf3f11
subashininagalingam <subanagalingam.dev@gmail.com>
2026-04-13 15:52:09 +0530

    Implement a student list page

--- myapp/student_model/urls.py
+++ myapp/student_model/urls.py
@@ -3,6 +3,9 @@ from . import views
 
 urlpatterns = [
     path('',views.student,name='student'),
-    path('student_list/',views.student_list,name='student_list')
+    path('success/',views.success,name='success'),
+    path('student_list/',views.student_list,name='student_list'),
+    path('edit_student/<int:id>/',views.edit_student,name='edit_student'),
+    path('delete_student/<int:id>/',views.delete_student,name='delete_student'),
     
 ]
--- myapp/student_model/views.py
+++ myapp/student_model/views.py
@@ -1,7 +1,8 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from .forms import StudentForm
 from .models import *
+from django.contrib import messages
 
 # Create your views here.
 def student(request):
@@ -9,13 +10,37 @@ def student(request):
         form=StudentForm(request.POST)
         if(form.is_valid):
             form.save()
-            return HttpResponse("Student Registered Successfully")
+            return redirect('success')
     else:
         form=StudentForm()
 
     return render(request,"student_model/register.html",{'form': form})
 
+def success(request):
+    return render(request,"student_model/success.html")
+
 def student_list(request):
     students=Student.objects.all()
     return render(request,"student_model/student_list.html",{'students':students})
+
+def edit_student(request, id):
+    student=Student.objects.get(id=id)
+    if (request.method=='POST'):
+        student.name=request.POST.get('name')
+        student.mobile_no=request.POST.get('mobile_no')
+        student.email=request.POST.get('email') 
+        student.dob=request.POST.get('dob')
+        student.address=request.POST.get('address')
+        student.save()
+        return redirect('student_list')
+    return render(request, "student_model/edit_student.html", {'student': student})
+
+        
+
+def delete_student(request, id):
+    student=Student.objects.get(id=id)
+    student.delete()
+    messages.success(request, "✅Student deleted successfully")
+    return redirect('student_list')
+
 
