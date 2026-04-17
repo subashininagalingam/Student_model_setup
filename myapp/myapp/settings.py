341fa239a5d5aed30d971e20b4f3fcdffdc362e3
subashininagalingam <subanagalingam.dev@gmail.com>
2026-04-17 13:33:31 +0530

    changes in settings.py

--- myapp/myapp/settings.py
+++ myapp/myapp/settings.py
@@ -120,7 +120,11 @@ USE_TZ = True
 
 ALLOWED_HOSTS = ['*']
 
-STATIC_URL = 'static/'
+import os
 
-MEDIA_URL = 'media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
